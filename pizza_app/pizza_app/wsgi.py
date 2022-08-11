14ee97a0c61dd5397bbbb3b83907554cbfd95b23
Dawid Nizioł <DAN@hansenpolska.eu>
2022-08-11 07:59:50 +0200

    Initial commit

---
+++ pizza_app/pizza_app/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pdrs project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+from dotenv import load_dotenv
+
+from django.core.wsgi import get_wsgi_application
+
+load_dotenv()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdrs.settings')
+
+application = get_wsgi_application()
