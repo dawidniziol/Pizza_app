32399af87fc6575f14caf364d9505df21a565601
DAN <dawid.niziol@gmail.com>
2022-08-11 09:40:26 +0200

    działa :D

--- pizza_app/pizza_app/settings.py
+++ pizza_app/pizza_app/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,7 +76,7 @@ WSGI_APPLICATION = 'pizza_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+print(dj_database_url.config())
 DATABASES = {"default": dj_database_url.config()}
 
 
